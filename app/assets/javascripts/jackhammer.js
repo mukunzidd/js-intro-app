e1f8f1028f3abda561b40ec6c6c1046296cc35e7
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-22 12:20:07 +0200

    Resolved the Jackhammer js.

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,16 @@
+// FIrst Question
+// function jackhammer(){
+// 	alert('YOU WIN HOSS!!!');
+// }
+
+// Second question
+var clicks = 0;
+function jackhammer(){
+	if (clicks === 4) {
+		alert('YOU WIN HOSS 5 Clicks!!!');
+	} else 
+	clicks++;
+	console.log('Way to go hoss!');
+	console.log(clicks);
+}
 
